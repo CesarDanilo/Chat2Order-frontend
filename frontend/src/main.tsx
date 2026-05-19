b1f05934dd8b56d26f8f3b1959aebd9f12f3376b
Cesar <billypalacios616@gmail.com>
2026-05-19 10:31:33 -0400

    feat: create new project

---
+++ frontend/src/main.tsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client'
+import {
+  createRouter,
+  RouterProvider,
+} from '@tanstack/react-router'
+
+import { routeTree } from './routeTree.gen'
+
+import './index.css'
+
+const router = createRouter({
+  routeTree,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+)
