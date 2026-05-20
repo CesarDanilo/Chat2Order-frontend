d0ed73ebe19b2b218a2a5b74ea25b5e63d4072a1
Cesar <billypalacios616@gmail.com>
2026-05-20 14:37:14 -0400

    chore: finalizando filtros

--- frontend/src/routes/orders.tsx
+++ frontend/src/routes/orders.tsx
@@ -5,7 +5,9 @@ import {
   InputGroupAddon,
   InputGroupInput,
 } from "@/components/ui/input-group"
-import { SearchIcon } from 'lucide-react'
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Button } from "@/components/ui/button"
+import { CirclePlus, SearchIcon } from 'lucide-react'
 
 export const Route = createFileRoute('/orders')({
   component: RouteComponent,
@@ -16,13 +18,28 @@ function RouteComponent() {
     <div className='w-screen'>
       <Header title={"Pedidos"} subtitle={"Todos os pedidos importados"} />
       <div className='px-56 py-10'>
-        <div className='filters'>
+        <div className='filters flex justify-around items-center'>
           <InputGroup className="w-[390px]">
             <InputGroupInput placeholder="Buscar por cliente ou ID" />
             <InputGroupAddon>
-              <SearchIcon />  
+              <SearchIcon />
             </InputGroupAddon>
           </InputGroup>
+
+          <Tabs defaultValue="Todos" className="w-[390px]">
+            <TabsList>
+              <TabsTrigger value="Todos" className="text-xs">Todos</TabsTrigger>
+              <TabsTrigger value="Pendentes" className="text-xs">Pendentes</TabsTrigger>
+              <TabsTrigger value="Concluídos" className="text-xs">Concluídos</TabsTrigger>
+              <TabsTrigger value="Cancelados" className="text-xs">Cancelados</TabsTrigger>
+            </TabsList>
+          </Tabs>
+
+          <Button className='text-xs cursor-pointer'>
+            <CirclePlus />
+            Novo pedido
+          </Button>
+
         </div>
       </div>
     </div>
