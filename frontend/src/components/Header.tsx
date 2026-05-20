768118aeac80b58a90e268b583014d6850039e7d
Cesar <billypalacios616@gmail.com>
2026-05-20 14:17:44 -0400

    chore: refatorando alinhamento de header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
 
 export function Header({ title, subtitle }: HeaderProps){
   return(
-  <div className="flex w-full justify-between px-10 py-5 border-r border-b">
+  <div className="flex w-full h-16 justify-between items-center px-8  border-r border-b">
     <div className="flex flex-col">
       <span className="text-sm font-semibold">{ title }</span>
       <span className="text-xs text-zinc-600">{ subtitle }</span>
--- frontend/src/routes/orders.tsx
+++ frontend/src/routes/orders.tsx
@@ -1,5 +1,11 @@
 import { Header } from '@/components/Header'
 import { createFileRoute } from '@tanstack/react-router'
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group"
+import { SearchIcon } from 'lucide-react'
 
 export const Route = createFileRoute('/orders')({
   component: RouteComponent,
@@ -9,6 +15,16 @@ function RouteComponent() {
   return (
     <div className='w-screen'>
       <Header title={"Pedidos"} subtitle={"Todos os pedidos importados"} />
+      <div className='px-56 py-10'>
+        <div className='filters'>
+          <InputGroup className="w-[390px]">
+            <InputGroupInput placeholder="Buscar por cliente ou ID" />
+            <InputGroupAddon>
+              <SearchIcon />  
+            </InputGroupAddon>
+          </InputGroup>
+        </div>
+      </div>
     </div>
   )
 }
